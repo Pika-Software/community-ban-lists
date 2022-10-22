5256b8b1dde646368247f6a3399a8a6cb53ebd0e
PrikolMen <olegis019@gmail.com>
2022-10-22 04:48:01 +0400

    Updated

--- lua/autorun/server/community_ban_lists.lua
+++ lua/autorun/server/community_ban_lists.lua
@@ -3,32 +3,45 @@ include( 'community_bans/core.lua' )
 
 community_bans.InstallLocal( 'community_bans/lists/' )
 
-hook.Add('CheckPassword', 'Community Ban Lists', function( steamID64, ip, _, __, nickname )
-    local banned, reason, silent, listName = community_bans.CheckSteamID64( steamID64 )
-    if (banned) then
-        local info = {
-            -- Player Info
-            ['nickname'] = nickname,
-            ['steamid64'] = steamID64,
-            ['ip'] = ip,
+do
 
-            -- Server Info
-            ['listname'] = listName,
-            ['serverip'] = game.GetIPAddress(),
-            ['servername'] = cvars.String( 'hostname', 'Garry\'s Mod' )
-        }
+    local community_bans_CheckSteamID64 = community_bans.CheckSteamID64
+    local community_bans_Log = community_bans.Log
+    local util_SteamIDFrom64 = util.SteamIDFrom64
+    local game_GetIPAddress = game.GetIPAddress
+    local cvars_String = cvars.String
+    local string_lower = string.lower
+    local string_gsub = string.gsub
+    local isstring = isstring
 
-        if not silent then
-            community_bans.Log( 'Player \'' .. nickname .. '\' (' .. steamID64 .. ') blocked in \'' .. listName .. '\'.' )
-        end
+    hook.Add('CheckPassword', 'Community Ban Lists', function( steamID64, ip, _, __, nickname )
+        local banned, reason, silent, listName = community_bans_CheckSteamID64( steamID64 )
+        if (banned) then
+            local info = {
+                -- Player Info
+                ['nickname'] = nickname,
+                ['steamid64'] = steamID64,
+                ['ip'] = ip,
 
-        return false, string.gsub( reason, '{(%w)}', function( str )
-            local value = info[ string.lower( str ) ]
-            if isstring( value ) then
-                return value
+                -- Server Info
+                ['listname'] = listName,
+                ['serverip'] = game_GetIPAddress(),
+                ['servername'] = cvars_String( 'hostname', 'Garry\'s Mod' )
+            }
+
+            if not silent then
+                community_bans_Log( 'Player \'' .. nickname .. '\' (' .. util_SteamIDFrom64( steamID64 ) .. ') blocked in \'' .. listName .. '\'.' )
             end
 
-            return '{' .. str .. '}'
-        end)
-    end
-end)
+            return false, string_gsub( reason, '{(%w)}', function( str )
+                local value = info[ string_lower( str ) ]
+                if isstring( value ) then
+                    return value
+                end
+
+                return '{' .. str .. '}'
+            end)
+        end
+    end)
+
+end
